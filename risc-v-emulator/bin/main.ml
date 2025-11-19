faa0ba1794805bf7462482dc1eb2d46f9718a383
Matthew Laterza <mvl32@cornell.edu>
2025-11-18 22:39:14 -0500

    added basic client functionality - file reading and printing output

--- risc-v-emulator/bin/main.ml
+++ risc-v-emulator/bin/main.ml
@@ -1,1 +1,32 @@
-let () = print_endline "Hello, World!"
+open Risc_v_emulator.Logic
+
+(*Prints the contents of [registers] in the format "x[index] = [value]"*)
+let print_registers () =
+  print_endline "\nRegister contents:";
+  Array.iteri (fun i v -> Printf.printf "x%-2d = %d\n" i v) registers
+
+(*Reads contents from [file] and returns a list of strings*)
+let read_lines file =
+  try
+    let channel = open_in file in
+    let rec loop acc =
+      match input_line channel with
+      | line -> loop (line :: acc)
+      | exception End_of_file ->
+          close_in channel;
+          List.rev acc
+    in
+    loop []
+  with Sys_error msg ->
+    print_endline ("Error opening file: " ^ file ^ "\n" ^ msg);
+    exit 1
+
+let () =
+  if Array.length Sys.argv < 2 then (
+    prerr_endline "Usage: dune exec risc-v-emulator -- <file.s>";
+    exit 1);
+
+  let filename = Sys.argv.(1) in
+  let instructions = read_lines filename in
+  (*[instructions] is sent to backend and [registers] is updated*)
+  print_registers ()
