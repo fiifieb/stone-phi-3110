7db3dc18849edd87c1d79420362e59ec734059dd
Matthew Laterza <mvl32@cornell.edu>
2025-11-19 13:39:16 -0500

    Implemented cpu initialization and creation of initial registers

--- risc-v-emulator/bin/main.ml
+++ risc-v-emulator/bin/main.ml
@@ -1,9 +1,60 @@
 open Risc_v_emulator.Logic
 
 (*Prints the contents of [registers] in the format "x[index] = [value]"*)
-let print_registers () =
+let print_registers regs =
   print_endline "\nRegister contents:";
-  Array.iteri (fun i v -> Printf.printf "x%-2d = %d\n" i v) registers
+  Array.iteri (fun i v -> Printf.printf "x%-2d = %d\n" i v) regs
+
+(*Parses a line starting with "IRV" and returns a list of initial
+  (register,value) pairs*)
+let parse_irv line : (int * int) list =
+  print_endline line;
+  let line = String.trim line in
+  let prefix = "IRV:" in
+  if
+    String.length line < String.length prefix
+    || String.sub line 0 (String.length prefix) <> prefix
+  then failwith "IRV line missing IRV prefix";
+  let line' =
+    String.sub line (String.length prefix)
+      (String.length line - String.length prefix)
+  in
+  let regs =
+    line' |> String.trim |> String.split_on_char ' '
+    |> List.filter (fun s -> s <> "")
+  in
+  List.map
+    (fun reg ->
+      match String.split_on_char '@' reg with
+      | [ reg; value ] ->
+          let r = int_of_string reg in
+          let v = int_of_string value in
+          (r, v)
+      | _ -> failwith ("Invalid IRV entry: " ^ reg))
+    regs
+
+(*Returns a [cpu_state] where [cpu_state.pc] is the number of instructions,
+  [cpu_state.regs] = the array of registers including initial values specified
+  in [lines], and [cpu_state.instrs] is the array of instructions created from
+  [lines]*)
+let cpu_init (lines : string list) : cpu_state =
+  match lines with
+  | [] -> failwith "empty input file"
+  | irv :: instructions ->
+      let registers = Array.make 32 0 in
+      let initial_vals = parse_irv irv in
+      List.iter
+        (fun (r, v) ->
+          if r < 0 || r > 31 then failwith "Register out of range"
+          else registers.(r) <- v)
+        initial_vals;
+      let instruction_list = make_instructions instructions in
+      let instruction_array = Array.of_list instruction_list in
+      {
+        pc = Array.length instruction_array;
+        regs = registers;
+        instrs = instruction_array;
+      }
 
 (*Reads contents from [file] and returns a list of strings*)
 let read_lines file =
@@ -27,28 +78,22 @@ let () =
     exit 1);
 
   let filename = Sys.argv.(1) in
-  let instructions = read_lines filename in
-  List.iteri (fun i s -> Printf.printf "%d: %s\n" (i + 1) s) instructions;
+  let lines = read_lines filename in
+  let cpu = cpu_init lines in
+  Printf.printf "Register Contents:\n";
+  print_registers cpu.regs
 
-  (*[instructions] is sent to backend and [registers] is updated*)
+(*[instructions] is sent to backend and [registers] is updated*)
 
-  (**TODO: The first line of the input file will contain some values to be put in
-    some registers. Consider this the initial state of the registers.
+(**TODO: The first line of the input file will contain some values to be put in
+   some registers. Consider this the initial state of the registers.
 
-    Eg. 
-        IRV: 8@0xbeef 2@0xbee 9@0xef
-        addi x8,x8,9 
-        add x1,x8,x9 
-        add x1,x1,x2
+   Eg. IRV: 8@0xbeef 2@0xbee 9@0xef addi x8,x8,9 add x1,x8,x9 add x1,x1,x2
 
-    So you'll have to create a [cpu_state]. the [pc] will be the number of
-    instructions in the instruction array. [regs] will be the array of 32
-    registers each initially set to 0. then you'll have to put the initial
-    register values (IRV) into the appropriate registers.
-    Then [instrs] will be the instruction array which you'll make by calling 
-    [make_instructions] with the list of instructions you read from the file 
-    excluding the first line (since the first line has the initial register 
-    values (IRV))
-
-     *)
-  print_registers ()
+   So you'll have to create a [cpu_state]. the [pc] will be the number of
+   instructions in the instruction array. [regs] will be the array of 32
+   registers each initially set to 0. then you'll have to put the initial
+   register values (IRV) into the appropriate registers. Then [instrs] will be
+   the instruction array which you'll make by calling [make_instructions] with
+   the list of instructions you read from the file excluding the first line
+   (since the first line has the initial register values (IRV)) *)
